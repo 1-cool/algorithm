91d5e11627783a3b1c9a6dfb6b69496659625363
1-cool <38838272+1-cool@users.noreply.github.com>
2018-11-13 22:51:46 +0800

    Update 油田问题.c

--- 油田问题.c
+++ 油田问题.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 char grid[101][101];//网格
-int n,m;//行、列
+int m,n;//行、列
 //一个网格的8个方向
 int dir[8][2]={{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
 
@@ -13,7 +13,7 @@ void DFS(int x,int y)
     {
         xx=x+dir[i][0];
         yy=y+dir[i][1];
-        if(xx<0||xx>=n||yy<0||yy>=m)
+        if(xx<0||xx>=m||yy<0||yy>=n)
             continue;//越界终止本次循环
         if(grid[xx][yy]=='@')
             DFS(xx,yy);//如果方格还是‘@’，则继续搜索
@@ -24,15 +24,15 @@ int main()
 {
     int i,j;//循环变量
     int count;//油田数量计数
-    while(scanf("%d%d",&n,&m)!=EOF)
+    while(scanf("%d%d",&m,&n)!=EOF)
     {
-        if(n==0&&m==0)
+        if(m==0&&n==0)
             break;
         count=0;
-        for(i=0;i<n;i++)
+        for(i=0;i<m;i++)
             scanf("%s",grid[i]);
-        for(i=0;i<n;i++)
-            for(j=0;j<m;j++)
+        for(i=0;i<m;i++)
+            for(j=0;j<n;j++)
                 if(grid[i][j]=='@')
                 {
                     DFS(i,j);count++;

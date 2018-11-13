bc664ef53b497bc679da11ab7e4dbb0e4222eff5
1-cool <38838272+1-cool@users.noreply.github.com>
2018-11-13 22:37:22 +0800

    Create 油田问题.c
    油田问题
    描述
    
    输入一个m行n列的字符矩阵，统计“@”组成多少个连通块。如果两个字符“@”所在的格子相邻（横，竖或者是对角线方向），就说它们属于同一个连通块。
    
    
    输入
    
    第一行输入两个数m和n。(1\le m,n\le 100)(1≤m,n≤100)
    接下来输入m行n列的字符矩阵（由"@"和" * "组成）。
    
    
    输出
    
    输出一个正整数，表示字符矩阵中“@”组成连通块的个数。
    
    
    输入样例 1 
    
    1 1
    *
    输出样例 1
    
    0
    输入样例 2 
    
    3 5 
    *@*@*
    **@** 
    *@*@*
    输出样例 2
    
    1

---
+++ 油田问题.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+char grid[101][101];//网格
+int n,m;//行、列
+//一个网格的8个方向
+int dir[8][2]={{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+
+//从（x,y）进行深度优先遍历
+void DFS(int x,int y)
+{
+    int xx,yy;
+    grid[x][y]='*';//将grid中访问过的方块设置成‘*’，防止重复访问~
+    for(int i=0;i<8;i++)
+    {
+        xx=x+dir[i][0];
+        yy=y+dir[i][1];
+        if(xx<0||xx>=n||yy<0||yy>=m)continue;//越界终止本次循环
+        if(grid[xx][yy]=='@')DFS(xx,yy);//如果方格还是‘@’，则继续搜索
+    }
+}
+
+int main()
+{
+    int i,j;//循环变量
+    int count;//油田数量计数
+    while(scanf("%d%d",&n,&m)!=EOF)
+    {
+        if(n==0&&m==0)break;
+            count=0;
+        //memset(grid,0,sizeof(grid));
+
+        for(i=0;i<n;i++)scanf("%s",grid[i]);
+            for(i=0;i<n;i++)
+                for(j=0;j<m;j++)
+                    if(grid[i][j]=='@'){DFS(i,j);count++;}//在（i,j）遍历，并且遍历了一个“油田”，计数器加1
+                        printf("%d\n",count);
+    }
+    return 0;
+}
